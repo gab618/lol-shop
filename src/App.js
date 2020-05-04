d981d93c740801834df1babd3e25500960bc7896
Gabriel <knuckles4x@gmail.com>
2020-05-04 16:49:54 -0300

    reactotron config

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Routes from './routes';
 
+import './config/ReactotronConfig';
+
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
