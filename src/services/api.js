2a606cdb642351a61f08162530ba7921d9ebf259
Gabriel <knuckles4x@gmail.com>
2020-05-08 18:26:14 -0300

    api address env var

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://knuckles4x.pythonanywhere.com/lolshop',
+  baseURL: `${process.env.REACT_APP_API_ADDRESS}`,
 });
 
 export default api;
